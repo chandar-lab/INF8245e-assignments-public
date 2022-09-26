a24e667b966e6550e3b99e0701a72c51456fb8c2
Gabriele Prato <25964820+pratogab@users.noreply.github.com>
2022-09-26 08:56:16 -0400

    Updated assignment 1 question 4b test, relaxed accepted range of values.

--- assignment_1_python/tests/question 4b.py
+++ assignment_1_python/tests/question 4b.py
@@ -3,18 +3,18 @@ OK_FORMAT = True
 test = {   'name': 'question 4b',
     'points': 20,
     'suites': [   {   'cases': [   {   'code': '>>> _train_loss, _val_loss, _w = ridge_regression_gradient_descent(\n'
-                                               '...     np.asarray([[0.5], [-0.5], [1.4]], dtype=np.float64),\n'
-                                               '...     np.asarray([[-0.3, 0.1], [0.2, 1.1], [-0.45, -0.77]], dtype=np.float64),\n'
-                                               '...     np.asarray([[0.76], [0.13], [-0.82]], dtype=np.float64),\n'
-                                               '...     np.asarray([[1.04, -0.4], [0.9, 0.1], [-0.5, 0.47]], dtype=np.float64),\n'
-                                               '...     np.asarray([[0.8], [-0.12]], dtype=np.float64),\n'
-                                               '...     0.99,\n'
-                                               '...     1e-7,\n'
+                                               '...     np.asarray([[0.5], [-0.5], [1.4]]),\n'
+                                               '...     np.asarray([[-0.3, 0.1], [0.2, 1.1], [-0.45, -0.77]]),\n'
+                                               '...     np.asarray([[0.76], [0.13], [-0.82]]),\n'
+                                               '...     np.asarray([[1.04, -0.4], [0.9, 0.1], [-0.5, 0.47]]),\n'
+                                               '...     np.asarray([[0.8], [-0.12]]),\n'
+                                               '...     0.90,\n'
+                                               '...     1e-3,\n'
                                                '...     1)\n'
                                                '>>> \n'
-                                               '>>> np.testing.assert_allclose(_train_loss, np.asarray([1.0992692], dtype=np.float32))\n'
-                                               '>>> np.testing.assert_allclose(_val_loss, np.asarray([0.40028697], dtype=np.float32))\n'
-                                               '>>> np.testing.assert_allclose(_w, np.asarray([[0.79999981], [-0.12000004]], dtype=np.float32))\n',
+                                               '>>> np.testing.assert_allclose(_train_loss, np.asarray([1.0992692]), rtol=1e-6)\n'
+                                               '>>> np.testing.assert_allclose(_val_loss, np.asarray([0.40028697]), rtol=1e-6)\n'
+                                               '>>> np.testing.assert_allclose(_w, np.asarray([[0.79819946], [-0.12038055]]), rtol=1e-6)\n',
                                        'hidden': False,
                                        'locked': False,
                                        'points': 2}],
