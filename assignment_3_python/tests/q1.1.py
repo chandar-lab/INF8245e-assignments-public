6fdfd1eb54ea0afa32091e62a005819044b8cace
Gonçalo Mordido <goncalomordido@gmail.com>
2022-10-26 15:00:18 -0400

    Added pandas in the test argument

--- assignment_3_python/tests/q1.1.py
+++ assignment_3_python/tests/q1.1.py
@@ -6,7 +6,7 @@ name = "q1.1"
 points = 2
 
 @test_case(points=0.5, hidden=False)
-def test_q1_1_public_1(numpy, train_data, val_data, test_data):
+def test_q1_1_public_1(numpy, pandas, train_data, val_data, test_data):
     numpy.testing.assert_allclose(isinstance(train_data["reviewText"], pandas.Series), 1)
     numpy.testing.assert_allclose(isinstance(val_data["reviewText"], pandas.Series), 1)
     numpy.testing.assert_allclose(isinstance(test_data["reviewText"], pandas.Series), 1)
