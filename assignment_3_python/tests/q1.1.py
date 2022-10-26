68ec93a947d6e836082b26e17026d02a1b36264d
Gonçalo Mordido <goncalomordido@gmail.com>
2022-10-26 15:39:28 -0400

    Fixed NoneType error

--- assignment_3_python/tests/q1.1.py
+++ assignment_3_python/tests/q1.1.py
@@ -12,6 +12,6 @@ def test_q1_1_public_1(numpy, pandas, train_data, val_data, test_data):
     numpy.testing.assert_allclose(isinstance(test_data["reviewText"], pandas.Series), 1)
 
 @test_case(points=0.5, hidden=False)
-def test_q1_1_public_2(numpy, train_data):
-    numpy.testing.assert_allclose("." in train_data.reviewText[0], 0)
+def test_q1_1_public_2(numpy, sentence):
+    numpy.testing.assert_allclose("." in sentence, 0)
 
