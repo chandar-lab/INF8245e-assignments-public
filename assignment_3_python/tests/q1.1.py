e3a425bcbf8b54d71f5d747281d2babd5e1710e3
Gonçalo Mordido <goncalomordido@gmail.com>
2022-10-26 15:34:15 -0400

    Added publuc test

--- assignment_3_python/tests/q1.1.py
+++ assignment_3_python/tests/q1.1.py
@@ -5,9 +5,13 @@ OK_FORMAT = False
 name = "q1.1"
 points = 2
 
-@test_case(points=0.5, hidden=False)
+@test_case(points=0.0, hidden=False)
 def test_q1_1_public_1(numpy, pandas, train_data, val_data, test_data):
     numpy.testing.assert_allclose(isinstance(train_data["reviewText"], pandas.Series), 1)
     numpy.testing.assert_allclose(isinstance(val_data["reviewText"], pandas.Series), 1)
     numpy.testing.assert_allclose(isinstance(test_data["reviewText"], pandas.Series), 1)
+
+@test_case(points=0.5, hidden=False)
+def test_q1_1_public_2(train_data):
+    numpy.testing.assert_allclose("." in train_data.reviewText[0], 0)
 
